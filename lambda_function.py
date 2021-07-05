61508f8fe95db541fda1de54ad9605f48d5fd5e8
nigh7sh4de <plotnikd@gmail.com>
2021-07-05 15:12:25 -0700

    random word

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,12 @@
 import json
+from random_word import RandomWords
 
 def lambda_handler(event, context):
+
+    word = r.get_random_word(includePartOfSpeech="noun")
+
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Fuck your face :D')
+        'body': json.dumps({'word': word})
     }
